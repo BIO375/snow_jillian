dfaf23ae9a794cf27bfd5a68520fd21249230502
jilliansnow <jilliansnow1@gmail.com>
2019-10-01 08:17:36 -0400

    T-test lab

--- Assignments/T-test_2019-09-24.R
+++ Assignments/T-test_2019-09-24.R
@@ -32,19 +32,13 @@ library("tidyverse")
 tidyverse_update()
   
 data01 <- read_csv("datasets/abd/chapter12/chap12e3HornedLizards.csv")
-data01 <- %>% slice (-105)
-
-The code u gave above would not slice the 105 N/A observation in the dataset so I put the dataset into excel and took out the n/a myself as a way of working around the problem.  
-This is the dataset I made (ignore the X3 column <-doesnt affect anything):
-  
-library(readr)
-data01 <- read_csv("datasets/demos/data01.csv")
-View(data01)
+data01 <- data01 %>% slice (-105)
 
 summ_data01 <- data01 %>%
   group_by(Survival) %>% 
   summarise(mean_squamosalHornLength = mean(squamosalHornLength),
             median_squamosalHornLength = median(squamosalHornLength),
+            IQR_squamosalHornLength = IQR(squamosalHornLength),
             sd_squamosalHornLength = sd(squamosalHornLength),
             var_squamosalHornLength = var(squamosalHornLength))
 
